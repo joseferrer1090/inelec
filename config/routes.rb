a2779691d524af6917fcb16bec749cfef6f64354
Jose Carlos Ferrer Bermudez <jcfb90@gmail.com>
2017-03-31 16:11:57 -0500

    Proyecto inelec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'home#index'
+
+  #Routes NameSpace Login
+    namespace "auth" do
+      get '/login/', to:'authenticate#index', as:'_login'
+      get '/recovery/', to:'authenticate#recovery', as:'_recovery'
+    end
+  #Fin
+
+  #Routes NameSpace Administrator
+    namespace "admin" do
+      get '/administrator', to:'administrator#index', as:'index'
+    end
+  #Fin
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end

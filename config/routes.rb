d46c456fe379eecfafc3d5ff4856af41893a72a9
Jose Carlos Ferrer Bermudez <jcfb90@gmail.com>
2017-04-03 15:08:01 -0500

    Creando las Paginas about entre otras

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-
+  
   root to:'home#index'
 
+  get '/about', :to => 'home#about', as:'about'
   #Routes NameSpace Login
     namespace "auth" do
       get '/login/', to:'authenticate#index', as:'_login'
       get '/recovery/', to:'authenticate#recovery', as:'_recovery'
-    end
+      end
   #Fin
 
   #Routes NameSpace Administrator
@@ -14,7 +15,5 @@ Rails.application.routes.draw do
       get '/administrator', to:'administrator#index', as:'index'
     end
   #Fin
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end

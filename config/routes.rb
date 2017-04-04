eb3e2e6d3a1c8cd0f91622c465d75a7ff98678b3
Jose Carlos Ferrer Bermudez <jcfb90@gmail.com>
2017-04-04 15:16:33 -0500

    Empezando las vistas del dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root to:'home#index'
 
   get '/about', :to => 'home#about', as:'about'
+  
   #Routes NameSpace Login
     namespace "auth" do
       get '/login/', to:'authenticate#index', as:'_login'
-      get '/recovery/', to:'authenticate#recovery', as:'_recovery'
+      get '/recovery', to:'authenticate#recovery', as:'_recovery'
       end
   #Fin
 
@@ -15,5 +16,6 @@ Rails.application.routes.draw do
       get '/administrator', to:'administrator#index', as:'index'
     end
   #Fin
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
